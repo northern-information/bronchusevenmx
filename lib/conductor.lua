9719247dbeaefcca7a0262bf737795a76692d0dc
Ryan Laws <ryanlaws@gmail.com>
2021-08-29 09:23:57 -0700

    Use loop increment for rate in conductor

--- lib/conductor.lua
+++ lib/conductor.lua
@@ -13,14 +13,16 @@ function conductor.init()
   end
 
   -- "SCORE" DATA --
+  local rates = {1/4, 3/8, 1/2, 3/4, 1, 3/2, 2}
   conductor.test = Sequins{1, 2}
   -- just-intonation (perfect) fifth sequence
-  conductor.rate = table_stream{1/4, 3/8, 1/2, 3/4, 1, 3/2, 2}
+  conductor.rate = loop_stream(table_stream(rates), #rates, 3)
   conductor.offset_stream = series_stream(0, 0.001)
   conductor.amp_stream = loop_stream(series_stream(0.3, 0.05), 13)
   conductor.clockdiv_div_stream = loop_stream(series_stream(1, 1), 7)
-  conductor.clockdiv_stream = loop_stream(once_stream(), 
+  conductor.burst_stream = loop_stream(once_stream(), 
     conductor.clockdiv_div_stream.next())
+  conductor.changerate_stream = loop_stream(once_stream(), 70)
   conductor.decay_stream = loop_stream(series_stream(0.05, 0.05), 11)
 end
 
@@ -39,14 +41,18 @@ function conductor:act()
     -- local name = 'uneven-structure-' .. now_playing .. '.wav'
     -- print("now_playing "..name)
     self.sampler[now_playing]:scrub(
-      self.rate.next(), 
+      self.rate.last(), 
       self.offset_stream.next(),
       self.amp_stream.next())
   end
 
+  if self.changerate_stream.next() then
+      self.rate.next()
+  end
+
   -- it'd be cool to "auto-bind" once stream to another for e.g. looping
-  if a % 100 > 50 and self.clockdiv_stream.next() then
-    self.clockdiv_stream.set_length(conductor.clockdiv_div_stream.next())
+  if a % 100 > 50 and self.burst_stream.next() then
+    self.burst_stream.set_length(conductor.clockdiv_div_stream.next())
     -- local name = 'uneven-structure-' .. now_playing .. '.wav'
     -- function Sample:scrub(rate, start, amp)
     --self.sampler[now_playing]:scrub(1, self.offset_stream.next(), 1)
